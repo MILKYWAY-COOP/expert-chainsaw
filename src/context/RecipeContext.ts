ee089fdeae68f96cb3defc9e3a3f7c9113ae2d37
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-16 16:43:26 +0300

    Deletions

--- src/context/RecipeContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-import { spoonacularContextProps } from 'types/recipe';
-
-export const SpoonacularContext = createContext<spoonacularContextProps>({
-  recipes: [],
-  setRecipes: () => {}
-});
