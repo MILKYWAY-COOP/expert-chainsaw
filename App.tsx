8b2ea921a3e26194c80b43fd0d80f8e48cb45589
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-16 16:33:25 +0300

    modified App component

--- App.tsx
+++ App.tsx
@@ -1,47 +1,25 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import * as React from 'react';
+
+import StateProvider from './src/context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen, RecipeDetailScreen } from './src/screens';
 
-interface HomeScreenProps {
-  navigation: {
-    navigate: (name: string) => void;
-  };
-}
+const Stack = createStackNavigator();
 
-const Stack = createStackNavigator<{
-  Home: undefined;
-  Details: undefined;
-}>();
-
-const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title='Go to Details'
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-};
-
-const DetailsScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button title='Go Back' onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-};
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Details' component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StateProvider>
+      <>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='RecipeDetail' component={RecipeDetailScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+    </StateProvider>
   );
 }
+
+export default App;
