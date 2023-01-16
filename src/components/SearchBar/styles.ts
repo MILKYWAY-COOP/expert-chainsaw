ce6455ce31e71fe7703da422ebcd088e572191e4
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-16 16:42:31 +0300

    Added styles for Search Component

---
+++ src/components/SearchBar/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {},
+  input: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    fontSize: 18,
+    alignSelf: 'center'
+  }
+});
