c3314766e7248d57c762eb905288f1227bf0e775
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-19 14:59:24 +0300

    added navigation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View} from 'react-native';
+import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { SearchBar, RecipeCard } from '../../components';
+import { useApp } from '../../context';
 import styles from './styles';
 
 export default function Home() {
+  const { setRecipeId } = useApp();
+  const navigation = useNavigation();
+
+  const handleContainerPress = (id: number) => {
+    setRecipeId(id);
+    navigation.navigate('Details');
+  };
+
   return (
-    <View style={styles.container}>
-      <SearchBar />
-      <RecipeCard />
-    </View>
+    <>
+      <View style={styles.container}>
+        <SearchBar />
+        <RecipeCard handleContainerPress={handleContainerPress} />
+      </View>
+    </>
   );
 }
