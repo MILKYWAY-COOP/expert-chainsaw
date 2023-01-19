4ea5a8a6ba625cff721e670f7d40122ba617dca0
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-19 14:56:22 +0300

    added styles

--- App.tsx
+++ App.tsx
@@ -1,25 +1,46 @@
 import * as React from 'react';
-
-import StateProvider from './src/context';
+import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen, RecipeDetailScreen } from './src/screens';
+import { StatusBar } from 'expo-status-bar';
 
-const Stack = createStackNavigator();
+import Navigation from './src/navigation';
+import StateProvider from './src/context';
 
 function App() {
   return (
-    <StateProvider>
-      <>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName='Home'>
-            <Stack.Screen name='Home' component={HomeScreen} />
-            <Stack.Screen name='RecipeDetail' component={RecipeDetailScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </>
-    </StateProvider>
+    <View style={styles.container}>
+      <StatusBar
+        style='dark'
+        animated={true}
+        backgroundColor='yellow'
+        hidden={false}
+      />
+      <StateProvider>
+        <>
+          <NavigationContainer>
+            <Navigation />
+          </NavigationContainer>
+        </>
+      </StateProvider>
+    </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#FFFFE0',
+    paddingTop: 50,
+    paddingHorizontal: 20
+  },
+  buttonsContainer: {
+    padding: 10
+  },
+  textStyle: {
+    textAlign: 'center',
+    marginBottom: 8
+  }
+});
