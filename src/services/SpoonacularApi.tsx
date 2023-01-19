28f044679cb3d30282231674958caf73b4328677
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-19 15:01:15 +0300

    added a try catch block

--- src/services/SpoonacularApi.tsx
+++ src/services/SpoonacularApi.tsx
@@ -6,26 +6,38 @@ export const SpoonacularApi = () => {
   const [recipe, setRecipe] = useState<Recipe>();
   const [recipeId, setRecipeId] = useState<number>();
   const [recipeInfo, setRecipeInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
   const API_KEY = '70083fa828cd4a9db27aa185ec664e95';
 
   const search = async (searchTerm: string) => {
-    const response = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&apiKey=${API_KEY}`
-    );
-    const data = await response.json();
-    setRecipe(data);
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&apiKey=${API_KEY}`
+      );
+      const data = await response.json();
+      setRecipe(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
+    if (!recipeId) return;
     const getRecipeInfo = async () => {
-      const response = await fetch(
-        `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`
-      );
-      const data = await response.json();
-      setRecipeInfo(data);
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`
+        );
+        const data = await response.json();
+        setRecipeInfo(data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
     };
     getRecipeInfo();
   }, [recipeId]);
 
-  return { search, recipe, setRecipeId, recipeInfo };
+  return { search, recipe, setRecipeId, recipeInfo, loading };
 };
