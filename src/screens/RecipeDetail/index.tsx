bfd5206bb23d9139db43e5b80128c041bca404fe
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-19 15:00:00 +0300

    Added recipe details

--- src/screens/RecipeDetail/index.tsx
+++ src/screens/RecipeDetail/index.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, FlatList } from 'react-native';
 
+import { useApp } from '../../context';
 import styles from './styles';
 
 export default function RecipeDetail() {
+  const { recipeInfo: info, loading } = useApp();
+
+  const removeHTMLTags = (text: string) => {
+    if (!text) return '';
+    return text.replace(/(<([^>]+)>)/gi, '');
+  };
+
+  const summary = removeHTMLTags(info?.summary);
+  const instructions = removeHTMLTags(info?.instructions);
+
   return (
-    <View style={styles.container}>
-      <Text>RecipeDetail</Text>
-    </View>
+    <>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <ScrollView style={styles.container}>
+          <Text style={styles.title}>{info?.title}</Text>
+          <View>
+            <Text style={styles.h1}>Dishes</Text>
+            <FlatList
+              data={info?.dishTypes}
+              keyExtractor={(item) => item}
+              renderItem={({ item }) => (
+                <Text style={styles.dishTypes}>{item}</Text>
+              )}
+            />
+          </View>
+          <View>
+            <Text style={styles.h1}>Ingredients</Text>
+            {info.extendedIngredients?.map((ingredient) => (
+              <Text key={ingredient.id} style={styles.extendedIngredients}>
+                {ingredient?.original}
+              </Text>
+            ))}
+          </View>
+          <View>
+            <Text style={styles.h1}>Instructions</Text>
+            <Text style={styles.instructions}>{instructions}</Text>
+          </View>
+          <View>
+            <Text style={styles.h1}>Summary</Text>
+            <Text style={styles.summary}>{summary}</Text>
+          </View>
+        </ScrollView>
+      )}
+    </>
   );
 }
