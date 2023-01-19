e67dd34f5e4f3798e179e2b2e28b0c80d26b0f66
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-19 15:00:18 +0300

    modified styles

--- src/screens/RecipeDetail/styles.ts
+++ src/screens/RecipeDetail/styles.ts
@@ -2,7 +2,47 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#FF8C00'
+    backgroundColor: '#FFFFE0',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#3F51B5',
+    textAlign: 'center',
+    padding: 10
+  },
+
+  h1: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#3F51B5',
+    textAlign: 'center',
+    padding: 10
+  },
+
+  dishTypes: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#6D4C41'
+  },
+
+  extendedIngredients: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#6D4C41'
+  },
+
+  instructions: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#6D4C41'
+  },
+
+  summary: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#6D4C41',
+    marginBottom: 20
   }
 });
