1cd8efaf40818738f21f3f18e54b509d11a6d1d6
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-19 14:57:12 +0300

    added react navigation

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,26 +1,23 @@
 import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 import { useApp } from '../../context';
+import { IHandleContainerPress } from '../../types/recipe';
 import styles from './styles';
 
-export default function RecipeCard() {
-  const { recipe, setRecipeId } = useApp();
-  const navigation = useNavigation();
-
-  const handleContainerPress = (id: number) => {
-    setRecipeId(id);
-    navigation.navigate('RecipeDetail');
-  };
+export default function RecipeCard({
+  handleContainerPress
+}: IHandleContainerPress) {
+  const { recipe } = useApp();
 
   return (
-    <ScrollView>
+    <ScrollView style={styles.main}>
       <View style={styles.container}>
         {recipe?.results?.map((item, index) => {
           return (
             <TouchableOpacity
               key={index}
-              onPress={() => handleContainerPress(item.id)}
+              onPress={() => handleContainerPress(item?.id)}
+              style={styles.imageContainer}
             >
               <Image source={{ uri: item?.image }} style={styles.image} />
               <Text style={styles.title}>{item?.title}</Text>
